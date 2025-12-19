3d55dc37e62a6423cccd9e59faf5b665184da35b
divyaprabhakar <divyaprabhakar@qburst.com>
2025-12-20 00:11:07 +0530

    3sum-opposite-direction

---
+++ two_pointers/01-opposite_direction/3sum.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+}
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums) // Very Important
+	result := [][]int{}
+
+	if len(nums) == 0 {
+		return nil
+	}
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left := i + 1
+		right := len(nums) - 1
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				result = append(result, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return result
+}
